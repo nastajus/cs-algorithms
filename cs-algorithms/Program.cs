a1cf2de3777de6e6f0412974eb7c6f13c32d3b12
Ian Nastajus <ian.nastajus@gmail.com>
2018-04-13 11:01:16 -0400

    console algorithms

---
+++ cs-algorithms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cs_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort();
+        }
+
+        static void BubbleSort()
+        {
+
+        }
+
+        static void SelectionSort()
+        {
+
+        }
+
+//        static void TimeComplexityCounter()
+//        {
+//            
+//        }
+    }
+}
