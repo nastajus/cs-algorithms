b3e342106a92c9c8a4a5c15485cde2681ab4a286
Ian Nastajus <ian.nastajus@gmail.com>
2018-04-13 11:36:36 -0400

    simply playing around with syntax of iterations, arrays, static methods

--- cs-algorithms/Program.cs
+++ cs-algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace cs_algorithms
 {
@@ -6,12 +7,32 @@ namespace cs_algorithms
     {
         static void Main(string[] args)
         {
-            BubbleSort();
+            BubbleSort(new int[] { 4, 3, 2, 1 });
+            //BubbleSort({ 4, 3, 2, 1 });   //invalid [{..} not an expression, expression expected inside brackets]
+            BubbleSort(new int[3]); //valid 
+            BubbleSort(new int[0]);
+            ReadKey();
         }
 
-        static void BubbleSort()
+        static int[] BubbleSort(int[] arr)
         {
+            //for loops on List are a bit more than 2 times cheaper than foreach loops on List.
+            //Looping on array is around 2 times cheaper than looping on List.
 
+            Write(arr + " ");
+            foreach (var item in arr)
+            {
+                Write(item + " ");
+            }
+            WriteLine("");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Write(arr[i] + " ");
+            }
+            WriteLine("");
+
+            return new int[0];
         }
 
         static void SelectionSort()
