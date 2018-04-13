cdf99a1d969d749df59d00ba6fdbf234f9bb0bfd
Ian Nastajus <ian.nastajus@gmail.com>
2018-04-13 11:57:40 -0400

    by copy reconfirmed

--- cs-algorithms/Program.cs
+++ cs-algorithms/Program.cs
@@ -7,6 +7,7 @@ namespace cs_algorithms
     {
         static void Main(string[] args)
         {
+            BubbleSort(new int[] { 2, 1 });
             BubbleSort(new int[] { 4, 3, 2, 1 });
             //BubbleSort({ 4, 3, 2, 1 });   //invalid [{..} not an expression, expression expected inside brackets]
             BubbleSort(new int[3]); //valid 
@@ -14,25 +15,32 @@ namespace cs_algorithms
             ReadKey();
         }
 
-        static int[] BubbleSort(int[] arr)
+        static int[] BubbleSort(int[] array)
         {
-            //for loops on List are a bit more than 2 times cheaper than foreach loops on List.
-            //Looping on array is around 2 times cheaper than looping on List.
+            if (array.Length <= 1) return array;
 
-            Write(arr + " ");
-            foreach (var item in arr)
+            int[] sortedArray = new int[array.Length];
+
+            //suppose 2 items passed in, new int[] { 2, 1 }
+
+            for (int i = 0; i < array.Length - 1; i++)  //minus one here because below we look-ahead by one.
+            //              ^^^^^^^^^^^^^^^^
+            //              while "i" is less than ... is true, we execute the loop
             {
-                Write(item + " ");
-            }
-            WriteLine("");
+                var left = array[i];
+                var right = array[i + 1];
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Write(arr[i] + " ");
+                if (left > right)
+                {
+                    //swap (by reference?)
+                    var temp = right;
+                    right = left;   //by copy, lost
+                    left = temp;    //by copy, lost
+                }
             }
-            WriteLine("");
+            WriteLine("Sorted: " + Utils.PrintArray(sortedArray));
 
-            return new int[0];
+            return sortedArray;
         }
 
         static void SelectionSort()
@@ -44,5 +52,18 @@ namespace cs_algorithms
 //        {
 //            
 //        }
+    }
+
+    class Utils
+    {
+        public static string PrintArray(int[] array)
+        {
+            string result = "";
+            foreach (var item in array)
+            {
+                result += item + " ";
+            }
+            return result;
+        }
     }
 }
