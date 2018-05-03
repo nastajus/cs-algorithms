765eb4492d1a5e3a5af1401d4aef3fb22f3e2851
Ian Nastajus <ian.nastajus@gmail.com>
2018-05-03 01:51:00 -0400

    data structures: tasks -- added.

---
+++ cs-data-structures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cs_data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks();
+        }
+
+        static void Tasks()
+        {
+
+        }
+    }
+}
