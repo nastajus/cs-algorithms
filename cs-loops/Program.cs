4c471ef800cb5ddabe9e3df396f50c3d656a0380
Ian Nastajus <ian.nastajus@gmail.com>
2018-05-08 16:51:19 -0400

    various loop iteration types, over array, list, and dictionary

---
+++ cs-loops/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cs_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IteratingAndPrintingAnArrayOrCollection();
+            Console.ReadLine();
+
+        }
+
+        static void IteratingAndPrintingAnArrayOrCollection()
+        {
+            int[] arr = new[] { 1, 5, 3, 4, 2 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+
+            List<int> list = new List<int>(arr);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+
+            Dictionary<int, string> map = new Dictionary<int, string>
+            {
+                {1, "un"}
+            };
+            map.Add(4, "quatre");
+            map.Add(2, "deux");
+
+            foreach (var item in map)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+
+            foreach (KeyValuePair<int, string> entry in map)
+            {
+                Console.Write(entry);
+            }
+            Console.WriteLine();
+
+
+            foreach (var item in map)
+            {
+                Console.Write("Key: " + item.Key + " ");
+                Console.Write("Value: " + item.Value + "   ");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
