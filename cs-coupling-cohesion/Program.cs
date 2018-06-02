d07d6aaad9ffa0a4aecc250cdfc0f2e418577c10
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-02 16:21:47 -0400

    coupling cohesion basis

---
+++ cs-coupling-cohesion/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_coupling_cohesion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class rottweiler : Dog
+    {
+
+    }
+
+    class Dog : Animal
+    {
+
+    }
+
+    class Animal : Creature
+    {
+
+    }
+
+    class Creature
+    {
+
+    }
+
+    class Insect : Creature
+    {
+
+    }
+
+    class Ladybug : Insect
+    {
+
+    }
+
+    interface IGroomable
+    {
+        bool Groom();
+    }
+
+
+}
