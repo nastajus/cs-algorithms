83cc1b24c538f02606b8b8c324573f764848c83c
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-14 20:37:54 -0400

    attempt to unwrap box of kittens generically by exploring intellisense & my vague memories poorly.

---
+++ cs-generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Box { }
+        public class Kittens { }
+
+        //public void Unwrap (Box<Kittens> boxOfKittens) { }  //Error: The non-generic type 'Box' cannot be used with type arguments.
+
+        //public T Unwrap (Box<T> boxThings) { return new T(); } //Error: The non-generic type 'Box' cannot be used with type arguments.   //Error: The type or namespace 'T' cannot be found.
+
+        //public T Unwrap(Box<T> boxOfKittens) as T { }
+
+    }
+}
