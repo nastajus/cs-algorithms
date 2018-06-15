d207075d95dc580c47e708430a6299f950d07bd9
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-15 00:22:40 -0400

    cleanup generics section

--- cs-generics/Program.cs
+++ cs-generics/Program.cs
@@ -10,76 +10,10 @@ namespace cs_generics
     {
         static void Main(string[] args)
         {
-            var book = new Book { Isbn = "1111", Title = "C# Advanced" };
-
-            //var numbers = new List();
-            //numbers.Add(10);
-
-            //var books = new BookList();
-            //books.Add(book);
-
-            var numbers = new GenericList<int>();
-            numbers.Add(10);
-
-            var books = new GenericList<Book>();
-            books.Add(book);
         }
     }
 
-    class List
-    {
-        public void Add(int number)
-        {
-            throw new NotImplementedException();
-        }
+    interface IFlyable { }
+    interface IFlyable<TCargo> { }
 
-        //int get this[int index] {
-        //    get {throw new NotImplementedException(); }
-        //}
-
-        int At(int index)
-        {
-            return { throw new NotImplementedException () };
-        }
-
-    }
-
-    class BookList
-    {
-        public void Add(Book book) {
-            throw new NotImplementedException();
-        }
-
-        //Book get this[Book index ] {
-        //    get {throw new NotImplementedException(); }
-        //}
-
-        Book At(Book book)
-        {
-            return { throw new NotImplementedException(); }
-        }
-    }
-
-    class Book {
-        public string Isbn;
-        public string Title;
-    }
-
-    class GenericList<T>
-    {
-        public void Add(T value)
-        {
-
-        }
-
-        //public T this[int value]
-        //{
-
-        //}
-
-        public T At(int index)
-        {
-
-        }
-    }
 }
--- cs-generics/readme.md
+++
@@ -1,4 +0,0 @@
-https://www.youtube.com/watch?v=gyal6TbgmSU
-C# Generics Tutorial: Whats and Whys
-Programming with Mosh
-Published on May 1, 2015
