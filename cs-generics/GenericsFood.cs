1b67b3a55c3e5b5f4b95d7f21d0e1de34bff9e39
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-16 02:58:36 -0400

    creature edible interfaces generically attempted, has flaws.

---
+++ cs-generics/GenericsFood.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_generics
+{
+    class GenericsFood
+    {
+        //main
+    }
+
+    //class Food { }
+
+    /*
+    class Food
+    {
+        //protein
+        //carb
+        //fat
+        //etc.
+    }
+    */
+
+    //interface IEdible { }
+    //interface ICreature { }
+
+    interface IEdible<TEdible>
+    {
+
+    }
+
+    interface ICreature<TEdible>
+    {
+        void Eats(TEdible edible);      //probably or possibly a bad usage of TEdible??
+                                        //decidedly. doesn't follow principles in {cs_generics} 
+    }
+
+    /*
+    class Edible { }
+    class Waste { }
+    class Person { }
+    class Animal { }
+    */
+
+    //class Person : ICreature<>
+
+}
