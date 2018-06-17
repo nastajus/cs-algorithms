cd7372f7f9635ffff3f463e583272e501ee1369c
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-17 10:13:27 -0400

    just added second role of beer-drinker, but crammed into singular Person class. Great candidate for splitting out two interfaces now.

--- cs-polymorphsim/Polymorphism_Program.cs
+++ cs-polymorphsim/Polymorphism_Program.cs
@@ -12,34 +12,121 @@ namespace cs_polymorphsim
         {
             Doctor Riviera = new Doctor();
             Patient Homer = new Patient();
+            Drunkard Barney = new Drunkard();
+            Sicky Burns = new Sicky();
+            Bartender Moe = new Bartender();
+
 
             Riviera.Examine(Homer);
         }
 
         abstract class Person
         {
-            public abstract void Breath();
-            public abstract void Cough();
-            public abstract void Peer();
+            public abstract bool? Breath();
+            public abstract Phlegm Cough();
+            public abstract bool Peer();
+
+            public abstract bool AcceptBeer(Beer beer);
 
         }
 
+        class Bartender : Person
+        {
+            public Beer ServeBeerTo(Person person)
+            {
+
+            }
+
+            public override bool? Breath()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override Phlegm Cough()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override bool Peer()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class Beer { public string Vintage;}
+
+        class Phlegm { public string Description; }
+
+        class Sicky : Person {
+
+            public override bool? Breath()
+            {
+                return false;
+            }
+
+            public override Phlegm Cough()
+            {
+                return new Phlegm { Description = "Yucky and Green" };
+            }
+
+            public override bool Peer()
+            {
+                return false;
+            }
+        }
+
+        class Drunkard : Person
+        {
+            public override bool? Breath()
+            {
+                return true;
+            }
+
+            public override Phlegm Cough()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override bool Peer()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        class Baby : Person
+        {
+            public override bool? Breath()
+            {
+                return null;
+            }
+
+            public override Phlegm Cough()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override bool Peer()
+            {
+                return true;
+            }
+        }
 
         class Patient : Person
         {
-            public override void Breath()
+            public override bool? Breath()
             {
                 throw new NotImplementedException();
             }
 
-            public override void Cough()
+            public override Phlegm Cough()
             {
                 throw new NotImplementedException();
             }
 
-            public override void Peer()
+            public override bool Peer()
             {
-                throw new NotImplementedException();
+                return true;
             }
         }
 
@@ -55,17 +142,17 @@ namespace cs_polymorphsim
                 person.Peer();
             }
             
-            public override void Breath()
+            public override bool? Breath()
             {
                 throw new NotImplementedException();
             }
 
-            public override void Cough()
+            public override Phlegm Cough()
             {
                 throw new NotImplementedException();
             }
 
-            public override void Peer()
+            public override bool Peer()
             {
                 throw new NotImplementedException();
             }
