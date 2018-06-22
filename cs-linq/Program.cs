05a1749a4aa9904e4ce82dfaf05f5dcfdcecbde2
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-22 12:40:52 -0400

    linq for two lists comparing a sub-item returning matches in one of the lists, with khaled's help

--- cs-linq/Program.cs
+++ cs-linq/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -14,11 +15,45 @@ namespace cs_linq
             //purpose, examing why this isn't working as expected
             //var toUploadLogs = localLogData.Where(x => remoteLogs.Any(y => x.Date != y.Date));
 
-            List<Rat> xxx = new List<Rat>
+            List<Rat> rats = new List<Rat>
             {
                 new Rat { Number = 0, DTO = DateTimeOffset.Parse("2018-06-20")},
                 new Rat { Number = 1, DTO = DateTimeOffset.Parse("2018-06-21")},
             };
+            
+            List<Mice> mice = new List<Mice>
+            {
+                new Mice { Number = 1, DTO = DateTimeOffset.Parse("2018-06-21")},
+                new Mice { Number = 2, DTO = DateTimeOffset.Parse("2018-06-22")},
+            };
+
+            //rats.Aggregate()      //--> looking at autocomplete of Func<Rat,Rat,Rat> we realize it's not applicable. We aborted using this. 
+
+            //rats.SelectMany(r => r.) //-->autocomplete shows Func<Rat, IEnumerable<TCollection>> means I have to do a lambda, and pass in rat before =>, and return some kind of collection after the =>.   In any case, we aborted using this too.
+
+            var filteredRats = rats.Where(rat => mice.Any(m => m.DTO == rat.DTO));
+
+
+
+            IEnumerable<Rat> Fn(IEnumerable<Rat> irats, IReadOnlyCollection<Mice> imice)
+            {
+                foreach (var r in irats)
+                {
+                    if (imice.Any(m => m.DTO == r.DTO))
+                    {
+                        yield return r;
+                    }
+                }
+            }
+
+            var foo = Fn(rats, mice);
+
+
+
+
+
+
+
 
             List<Rat> yyy = new List<Rat>
             {
@@ -26,7 +61,8 @@ namespace cs_linq
                 new Rat { Number = 2, DTO = DateTimeOffset.Parse("2018-06-22")},
             };
 
-            var zzz = xxx.Where(x => yyy.Any(y => x.DTO != y.DTO));
+
+            var zzz = rats.Where(x => yyy.Any(y => x.DTO != y.DTO));
             //why doesn't this return { 0 and 2 } as expected, but instead returns null?
 
             //var zzz = xxx.Where(x => yyy.All(y => x.DTO != y.DTO));
@@ -41,16 +77,36 @@ namespace cs_linq
             //int[] Results = FirstArray.Intersect(SecondArray).ToArray();  
             //var zz = xxx.Intersect(yyy).Where()
 
+            //khaled says for transforming
+
             //like an inner join
-            var zz = xxx.Select(x => x.DTO).Intersect(yyy.Select(y => y.DTO)); //21 only
+            var zz = rats.Select(x => x.DTO).Intersect(yyy.Select(y => y.DTO)); //THE DATE OF 21 only
 
             //like a left join
-            var z = xxx.Select(x => x.DTO).Except(yyy.Select(y => y.DTO)); //20 only
+            var z = rats.Select(x => x.DTO).Except(yyy.Select(y => y.DTO)); //THE DATE OF 20 only
+
+            //how to get parent Rats?
+
+            // Find all the people older than 30
+            //var query1 = list.Where(person => person.Age > 30);
+            // Find each person's name
+            //var query2 = list.Select(person => person.Name);
+
+            //https://stackoverflow.com/questions/7187996/intersect-two-lists-in-c-sharp
+           // var newData = rats.Select(i => i.DTO).Intersect(yyy);
+
+
 
 
         }
 
         class Rat
+        {
+            public int Number { get; set; }
+            public DateTimeOffset DTO { get; set; }
+        }
+
+        class Mice
         {
             public int Number { get; set; }
             public DateTimeOffset DTO { get; set; }
