47dbf313fc74858575056e15a14a8213a3d6fea0
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-24 17:51:01 -0400

    strings and ints both use IComparable's CompareTo method
    but what i wonder is... what actually makes the CompareTo work... it's only an interface, and I cannot step into it... yet *something* happens with that

--- cs-generics/Generics_Mosh.cs
+++ cs-generics/Generics_Mosh.cs
@@ -12,6 +12,7 @@ namespace cs_generics
         {
             var f = Max(1, 2);
             var g = Max<int>(3, 4);
+            var h = Max<string>("cat", "dog");
         }
 
         public static int Max(int a, int b)
@@ -20,10 +21,13 @@ namespace cs_generics
         }
 
         public static T Max<T>(T a, T b) where T : IComparable
-        //                               ^^^^^^^^^^^^^^^^^^^^^
-        //                               constraint
         {
             if (typeof(T) == typeof(int))
+            {
+                return a.CompareTo(b) > 0 ? a : b;
+            }
+
+            if (typeof(T) == typeof(string))
             {
                 return a.CompareTo(b) > 0 ? a : b;
             }
