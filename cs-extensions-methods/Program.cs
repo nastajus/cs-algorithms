7ef0e79e056eb75a9838027a5c19d89174a7748b
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-24 14:38:30 -0400

    extension method added
    the question is when is it valuable and useful.

---
+++ cs-extensions-methods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Any;
+
+namespace cs_extensions_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rats = "rat king";
+            bool result; 
+            result = rats.IsBanana();
+            result = rats.IsString();
+            Console.ReadKey();
+        }
+    }
+}
+
+namespace Any
+{
+    static class Any
+    {
+        public static bool IsBanana(this string s)
+        {
+            if (s.GetType() == typeof(Banana))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsString(this string s)
+        {
+            if (s.GetType() == typeof(string))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    class Banana
+    {
+
+    }
+}
