b85dac8af23d72bfad24ce3b62389ca6baa4f9bd
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-24 16:08:00 -0400

    expressions vs. statements in conditional ternary

---
+++ cs-generics/Generics_Mosh.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_generics
+{
+    class Generics_Mosh
+    {
+        public int Max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        //  ^^^^^^^^^^^^^^^^^^^^^^^
+        //  statement
+        //         ^^^^^^^
+        //         expression
+        //                  ^ 
+        //                  expression
+        //                      ^
+        //                      expression
+            // operators? 
+            //      a : b 
+            //      which parts are really which types of syntactic scenarios? 
+            // eh. w/e.
+        }
+    }
+}
