0df8a66c8b10ccd67fd4fbe96f5f5463715549dc
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-24 15:29:16 -0400

    nope
    cannot convert concrete type to generic type

--- cs-generics/Generics_Kittens.cs
+++ cs-generics/Generics_Kittens.cs
@@ -15,12 +15,15 @@ namespace cs_generics
         }
     }
 
-    //useless
+
     class Box<TKitten>
     {
-        public Kitten Unwrap()
+        public TKitten Unwrap()
         {
+            //error: cannot implicitly convert type 'cs_generics.Kitten' to 'TKitten'
+            //error: cannot implicitly convert expression type 'cs_generics.Kitten' to 'TKitten'
             return new Kitten();
+
         }
     }
 
