26450adf965ce4374de78c20c2daaa173d0f8f6d
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-24 20:21:48 -0400

    ideation / some combinations... mostly isolated

--- cs-polymorphsim/Polymorphsim_Futurama.cs
+++ cs-polymorphsim/Polymorphsim_Futurama.cs
@@ -18,8 +18,20 @@ namespace cs_polymorphsim
     }
 
     /*
-     * Leela = Mutant
-     * Philip J Fry = TwentiethCentury
+     *
+     * i quickly find myself gravitating towards KINDS OF ACTIONS I want done...
+     *
+     * Presidents preside... (issue orders)
+     * DOOP issues orders too...
+     *      some inter-chain-of-command dank action ...
+     *
+     * Omicronions conquer planets...
+     *
+     * Mutants (live in sewers who cares... that's not an action)
+     *
+     * TwentiethCenturian's use Debit
+     * TwentyFirstCenturian's use BrainAds, Flying Tubes... Flyin cars...
+     * Philip J Fry 
      * Robots don't need air, can walk in space, like Bender, etc.
      * Amy's family uses riches to buy obscenely expensive things
      * Kip's, Zap Brannagon's army issues army commands
