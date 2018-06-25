1200283e905ff4ed955482162b3f120551d55188
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-24 20:47:00 -0400

    polymorphic interface of secret of mana much more defined
    things get weird hitting when getting close to a real-time inteface ... i'll need to stop prematurely  to finish something "nice"... otherwise i worry i'll devolve into bad designs fast.

--- cs-polymorphsim/Polymorphism_Secret_Of_Mana.cs
+++ cs-polymorphsim/Polymorphism_Secret_Of_Mana.cs
@@ -11,23 +11,50 @@ namespace cs_polymorphsim
     }
 
     /**
-     * Randi, Purim, and Popoi
+     * Randi, Purim, and Popoi : IPlayable, IFightable, IMerchant
+     *
+     * IPlayable
+     *      Move(Direction)
+     *      Press(Button) ~~ umm
+     *
+     * IFightable
+     *      Attack( ~~ whom ? ~~ Collision ??? umm )
+     *      Defend Chance ( ~~ property ~~ )
      *
      *
-     * IMagical
-     *      Cast(TSpell) 
+     *
+     * Purim, Popoi : IMagical
+     *      Cast(TSpell)
+     *      int MP;
      *
      * Fire Gigas, Spikey Tiger, Wall Face : IBoss 
-     *      Reward(TRewardable == e.g. Orb, ManaSeed)
+     *      Reward(TRewardable~~ == e.g. Orb, ManaSeed)
+     *      StoryAdvancement ~~
      *
+     * Green Slime, Goblin    : IUnderling
+     * Nitro Pumpkin, Spectre
+     * Chest
+     *      Drops(
+     *      GivesExperience(int, ? whom ? )
+     *      GivesMoney
      *
      * Jema : INPC
      *     Talk();
      *
-     * Shopkeeper : INPC, IMerchant
-     *      Talk()
+     * Neko, Shopkeeper : INPC, IMerchant
+     *      Talk(TWords)
      *      Buy(TTradable)
      *      Sell(TTradable)
+     *
+     * Neko : IOverpriced
+     *      { something that doubles all TTradable costs }
+     *
+     * Watts : IForge
+     *      Upgrade(TWeapon)
+     *
+     * Sword, Spear, Boomerang, Javalin, Whip, Axe, Knuckles, Bow & Arrow : IWeapons
+     *      Attack(IFightable)
+     *
      *
      */
 }
