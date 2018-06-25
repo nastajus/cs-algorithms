6f812ff648f86f516d7496f8d5c3f7326e8e6b58
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-24 20:30:04 -0400

    start secret of mana inteface

---
+++ cs-polymorphsim/Polymorphism_Secret_Of_Mana.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_polymorphsim
+{
+    class Polymorphism_Secret_Of_Mana
+    {
+    }
+
+    /**
+     * Randi, Purim, and Popoi
+     *
+     * Magic Users
+     *
+     * Thanatos
+     *
+     *
+     *
+     */
+}
