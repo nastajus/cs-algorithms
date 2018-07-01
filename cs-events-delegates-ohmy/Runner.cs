65500f977cf8863a177496f8ed8ef37a62d0d63d
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-01 19:02:59 -0400

    rename to Person

---
+++ cs-events-delegates-ohmy/Runner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_events_delegates_ohmy
+{
+    class Runner
+    {
+    }
+}
