5f3abf19372faae86e536172403e430e9ab180b8
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-01 18:59:36 -0400

    cleanup

--- cs-linq/Runner.cs
+++ cs-linq/Runner.cs
@@ -10,18 +10,6 @@ namespace cs_linq
     {
         static void Main(string[] args)
         {
-            RatsAndMice rm = new RatsAndMice();
-            rm.Run();
-
-            //compiler error:  Member 'RatsAndMice.Run()' cannot be accessed with an instance reference; qualify it with a type name instead
-
-                //rm is an instance
-                //.Run() in `rm.Run()` is *using* an instance reference (my words)
-
-                //qualify
-                //qualify it with a type name instead
-                //*use* an type name (my words)
-
             RatsAndMice.Run();
 
         }
