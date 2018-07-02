7659590b56afd0a683e29b9128674bc4d12c7b49
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-01 20:10:21 -0400

    rough rough Scheduler's User Property made...

--- cs-events-scheduler/Scheduler.cs
+++ cs-events-scheduler/Scheduler.cs
@@ -10,12 +10,42 @@ namespace cs_events_scheduler
     {
         static void Main(string[] args)
         {
+            Run();
+            Mock();
+        }
+
+        static void Run()
+        {
             //load up a list of existing schedules
             // listen for incoming `events` requests
             //  reply back with possibilities
             //   allow booking to different agencies, like Yoga teachers or Babysitters
             //    NOT use protobufs, nor GRPC. 
             //     these are to-be-run as native C# projects, to emphasize purely `events`
+
+        }
+
+        List<User> registeredUsers = new List<User>();
+
+        class Timeblock
+        {
+            User who;
+        }
+
+        class User
+        {
+            public string Name;
+            public string Phone;
+
+            public User(string name)
+            {
+                Name = name;
+            }
+        }
+
+        class Mock
+        {
+            
         }
     }
 }
