7b902dc92861e675fd4e310d90b062edec4908a8
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-02 12:33:16 -0400

    constraints ? ... nah...

--- cs-generics/Generics_Bookable.cs
+++ cs-generics/Generics_Bookable.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace cs_generics
 {
-    class Generics_Bookable<T>
+    class Generics_Bookable<T> where T : Bookable<T>
     {
         public static void Init()
         {
-            List<Bookable<T>> bookables = new List<Bookable<T>>();
-            //COMPILER ERROR: The **non-generic** list... cannot be used... wait a minute...
+            List<T> bookables = new List<T>();
+            //COMPILER ERROR: The **non-generic** list... cannot be used... with type arguments... wait a minute...
             bookables.Add<YogaRoom>(new YogaRoom());
         }
     }
