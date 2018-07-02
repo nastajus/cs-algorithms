ff45c29119e65808003b8ea7267243f825f4e881
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-02 12:43:50 -0400

    T of OMG

--- cs-generics/Generics_Bookable.cs
+++ cs-generics/Generics_Bookable.cs
@@ -6,13 +6,21 @@ using System.Threading.Tasks;
 
 namespace cs_generics
 {
+
+
+    // smouldering dumpster fire
+    // T of what?
+    // T of the one damn class I don't care about? 
+    // T of OMG.
+    //the logic ... isn't logical... time to die
+
     class Generics_Bookable<T>
     {
         public static void Init()
         {
             List<T> bookables = new List<T>();
             //COMPILER ERROR: The **non-generic** list... cannot be used... with type arguments... wait a minute...
-            bookables.Add<YogaRoom>(new YogaRoom());
+            bookables.Add<T>(new YogaRoom());
         }
     }
 
