a1059599f69a006f8886ac859eea5d5cc60611f0
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-02 12:30:27 -0400

    simple conversion problem isolated

--- cs-generics/Generics_Bookable.cs
+++ cs-generics/Generics_Bookable.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace cs_generics
 {
-    class Generics_Bookable
+    class Generics_Bookable<T>
     {
         public static void Init()
         {
-
+            List<Bookable<T>> bookables = new List<Bookable<T>>();
+            bookables.Add(new YogaRoom());
         }
+    }
+
+    class Bookable<T>
+    {
+
+    }
+
+    class YogaRoom
+    {
+        public string RoomName;
     }
 }
