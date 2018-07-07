3f5ff9568450c7def5193a95108b8a14672cdaf0
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-06 22:06:36 -0400

    domain vr hmd

---
+++ cs-domain-vr-hmd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_domain_vr_hmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    //runtime
+    class OVR { }
+    class OpenVR { }
+
+    //hardware
+    class Rift { }
+    class Vive { }
+
+
+
+}
