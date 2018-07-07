397f8631b909105065f11e31fe15bfa1626d2a8f
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-06 22:06:02 -0400

    event timer ... ideas ...

---
+++ cs-events-timer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_events_timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+    //request budget... e.g. fill job of x employees... or spend y dollars annually ... ugh 
+
+    //traffic accumulates at red lights for 30 seconds (5 sec)
+
+    //various requests come in, aggregate, then get executed... 
+
+    //.. some kind of information sharing between 
+    //  cookie monster & sesame street ..>? 
+    // boats? cars? ugh
+    // games related...hahaha... 
+
+}
