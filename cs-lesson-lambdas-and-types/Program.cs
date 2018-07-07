40ec9add7cd7676dc3c80b4715b81c00754213c2
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-07 11:11:36 -0400

    more...

--- cs-lesson-lambdas-and-types/Program.cs
+++ cs-lesson-lambdas-and-types/Program.cs
@@ -13,11 +13,21 @@ namespace cs_lesson_lambdas_and_types
         static void Main(string[] args)
         {
 
+            //depending on syntactics used... this occurs... 
+            //COMPILER ERROR: Only assignment, call, increment, decrement, and new object expressions can be used as a statement
+
             //() => { ; }
 
+            () => { Console.WriteLine("hi") };
+
             int _beta = 2;
-            (alpha) => { alpha + _beta; };
-            //COMPILER ERROR: Only assignment, call, increment, decrement, and new object expressions can be used as a statement
+            (alpha) => { alpha + _beta };
+
+            (string x, int y) => { y + x };
+
+            //(ref string x, int y) => { x = 23 } //um wait no.. not assignment...
+
+
         }
     }
 }
