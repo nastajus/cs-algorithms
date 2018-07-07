e484fa860954b3c58ee6b364ae4f2e0f6c33d780
Ian Nastajus <ian.nastajus@gmail.com>
2018-07-07 11:08:09 -0400

    lambdas and their types lesson proj started

---
+++ cs-lesson-lambdas-and-types/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_lesson_lambdas_and_types
+{
+    class Program
+    {
+        //useful tool: syntax visualizer
+
+        static void Main(string[] args)
+        {
+
+            //() => { ; }
+
+            int _beta = 2;
+            (alpha) => { alpha + _beta; };
+            //COMPILER ERROR: Only assignment, call, increment, decrement, and new object expressions can be used as a statement
+        }
+    }
+}
