7222295caeb9c92096f7f2f6581ce43affb37fdd
Ian Nastajus <ian.nastajus@gmail.com>
2018-08-07 13:40:34 -0400

    async explorations added

---
+++ cs-async/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            aaa();
+            Console.ReadKey();
+        }
+
+        static async Task<bool> aaa()
+        {
+            return await bbb();
+        }
+
+        static async Task<bool> bbb()
+        {
+            return false;
+        }
+    }
+}
