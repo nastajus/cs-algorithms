90bc55b0a3b9d1982b4bbd820e9e90c7070af73e
Ian Nastajus <ian.nastajus@gmail.com>
2018-08-07 16:21:00 -0400

    beware of async void in your code

---
+++ cs-async-void/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace cs_async_void
+{
+    /// <summary>
+    /// while attempting to reserach in google "Task<void>" or "Task<T> vs Task" return types,
+    /// came to low-fruit page about async void antipattern
+    /// so i'm doing that here.
+    /// https://theburningmonk.com/2012/10/c-beware-of-async-void-in-your-code/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer();
+            timer.Elapsed += OnTimerFired;
+            timer.Interval = 1000;
+            timer.Start();
+            Console.Read();
+        }
+
+        static async void OnTimerFired(object sender, ElapsedEventArgs args)
+        {
+            await Task.Delay(1000);
+
+            //kill process
+            throw new Exception();
+        }
+    }
+}
