66de3b77c2f8ed82818a9ab82bb2f7ab77dbe2f4
Ian Nastajus <ian.nastajus@gmail.com>
2018-08-22 15:22:05 -0400

    use lambda expression WITH { statement body }

--- cs-lambdas/Program.cs
+++ cs-lambdas/Program.cs
@@ -8,11 +8,20 @@ namespace cs_lambdas
 {
     class Program
     {
-        //follow along: https://www.dotnetperls.com/lambda
+        // follow along: https://www.dotnetperls.com/lambda
+
+        // expression vs statement:
+        // Expression: Something which evaluates to a value. Example: 1+2/x
+        // Statement:  A line of code which does something.  Example: GOTO 100
 
         static void Main(string[] args)
         {
+
             Func<int, int> func1 = x => x + 1;
+
+            //with a statement body...
+            //is it the braces? is it the *return* portion?
+            Func<int, int> func2 = x => { return x + 1; };
 
 
 
