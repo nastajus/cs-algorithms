a4a2229e211be2da88561e1ef72b5ff243523260
Ian Nastajus <ian.nastajus@gmail.com>
2018-08-22 15:03:53 -0400

    a lambda expression accepting predicate-style expression

--- cs-lambdas/Program.cs
+++ cs-lambdas/Program.cs
@@ -11,8 +11,13 @@ namespace cs_lambdas
         static void Main(string[] args)
         {
             //expression lambdas
+            List<int> elements = new List<int>() { 10, 20, 31, 40 };
+            int oddIndex = elements.FindIndex(x => x % 2 != 0);
+            Console.WriteLine(oddIndex);
 
             //statement lambdas
+            //...
+
         }
     }
 }
